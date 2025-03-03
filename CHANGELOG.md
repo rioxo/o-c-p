66993a261b00956bab04914df48a4a80b780dc47
Kimiko <khelies@reijo.xyz>
2025-03-03 12:04:06 +0100

    A.0 : Initialisation of the Project

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## A.0.0 - First Release
+* Generation of the package
---
+++ spec/o-c-p-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OCPView from '../lib/o-c-p-view';
+
+describe('OCPView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
